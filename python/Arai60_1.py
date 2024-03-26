bb6c8527dad3bd9ce37f41f442322610914e9d1f
inatoihs <sabanna.taihei@gmail.com>
2024-03-26 19:59:51 +0900

    代入を分けました

--- python/Arai60_1.py
+++ python/Arai60_1.py
@@ -9,7 +9,8 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fast = slow = head
         while fast and fast.next:
-            slow, fast = slow.next, fast.next.next
+            slow = slow.next
+            fast = fast.next.next
             if slow == fast:
                 return True
         return False
